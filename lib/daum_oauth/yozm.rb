def798fa4737a40f91674fcac919439ea7f0f3d9
Hoseong Hwang <thefron@wafflestudio.com>
2010-11-16 11:24:23 +0900

    added new methods for yozm

--- lib/daum_oauth/yozm.rb
+++ lib/daum_oauth/yozm.rb
@@ -17,15 +17,40 @@ module DaumOAuth
 				return get("/yozm/v1_0/user/show.json")
 			elsif options[:url_name]
 				return get("/yozm/v1_0/user/show.json?url_name=#{options[:url_name]}")
-			else options[:nick_name]
+			elsif options[:nick_name]
 				return get("/yozm/v1_0/user/show.json?nick_name=#{options[:nick_name]}")
+			else
+				return get("/yozm/v1_0/user/show.json")
 			end
 		end
 
 		#send a message to yozm 
-		def yozm_add(message, options={})
+		def yozm_add(message, options={}) #available options: url_shorten, img_url, parent_msg_id
 			#get("/yozm/v1_0/message/add.json?#{options.merge(:message => URI.encode(message)).collect{|k, v| "#{k}=#{v}"}.join('&')}")
 			post("/yozm/v1_0/message/add.json", options.merge(:message => message))
 		end
+
+		#check if a user joined yozm
+		def yozm_joined?
+			get("/yozm/v1_0/user/joined.json")
+		end
+
+		#join yozm
+		def yozm_join(nickname, options={})	#available options: sex(N/F/M), birthday(1984-11-14), blood_type(N/A/B/AB/O)
+			post("/yozm/v1_0/user/join.json", options.merge(:nickname => nickname))
+		end
+
+		def yozm_search(q, options={}) #available options: type(message - default, tag), page, count
+			get("/yozm/v1_0/timeline/search.json?#{options.merge(:q => q).collect{|k,v| "#{k}=#{v}"}.join('&')}")
+		end
+
+		def yozm_home_timeline(options={}) #available options: max_msg_id, since_msg_id, count
+			get("/yozm/v1_0/timeline/home.json?#{options.collect{|k,v| "#{k}=#{v}"}.join('&')}")
+		end
+
+		def yozm_user_timeline(url_name, options={}) #available options: next(=since_msg_id?, count)
+			get("/yozm/v1_0/timeline/user.json?#{options.merge(:url_name => url_name).collect{|k,v| "#{k}=#{v}"}.join('&')}")
+		end
+
 	end
 end
